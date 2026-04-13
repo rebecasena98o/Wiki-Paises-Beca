a0588257e67895fd15a5e76688b0e860881afb6c
Reb Sena <r3b3casena@gmail.com>
2026-04-13 15:58:06 -0300

    Feat: adicionando App e Main, configuração da página inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './style/StyleGlobal.css' // Importe o global aqui para testar
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,  
 )
