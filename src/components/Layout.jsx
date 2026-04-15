f69be79ccd9099458eadadb92d532af12f377569
Reb Sena <r3b3casena@gmail.com>
2026-04-14 22:58:59 -0300

    Feat: adicionando modo escuro

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,15 @@
 import Header from './Header'; 
 
-const Layout = ({ children, onFilter }) => {
+const Layout = ({ children, onFilter, toggleDarkMode, isDark }) => {
   return (
     <div className="app-container">
-      <Header onFilter={onFilter} />
-      <main>{children}</main>
+
+      <Header onFilter={onFilter} toggleDarkMode={toggleDarkMode} isDark={isDark} />
+
+      <main>
+        {children}
+      </main>
+
       <footer style={{ textAlign: 'center', padding: '20px', color: 'var(--text-secondary)' }}>
         <p>© 2026 WikiPaíses - Explore o Atlas</p>
       </footer>
