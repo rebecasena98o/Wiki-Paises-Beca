6e316116f41c153bc4f9e037083f572313b53dd0
Reb Sena <r3b3casena@gmail.com>
2026-04-14 23:04:47 -0300

    Feat: adicionando modo escuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import RoutesApp from "./routes/RoutesApp";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => setDarkMode(!darkMode);
+  
   return (
-    <RoutesApp/>
+    <RoutesApp toggleDarkMode={toggleDarkMode} isDark={darkMode} />
   );
 }
 
